8874fe215c068c6e654d6acc91250d6765e03d55
webstersolutions <webstersolutions.in@gmail.com>
2016-12-09 16:20:20 +0530

    Update index.jsx

--- server/web/map/index.jsx
+++ server/web/map/index.jsx
@@ -19,7 +19,7 @@ class MapPage extends React.Component {
                     
                     <div className="col-sm-12 text-center">
                         <a href="/">Home</a>
-                        <h1 className="page-header">Map</h1>
+                        <h1 className="page-header">Map View</h1>
                         <div>
                             <div id="map"></div>
                         </div>
